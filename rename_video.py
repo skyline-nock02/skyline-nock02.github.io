9abab18335272d952c4e459bfa19b9fea884c2b3
Pengsheng Guo <pengsheng_guo@apple.com>
2024-01-25 16:25:26 -0800

    update mp4

--- rename_video.py
+++ rename_video.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import json
+from moviepy.editor import VideoFileClip
 folder = 'assets/'
 prompt_mapping = {}
 for method_folder in glob.glob(os.path.join(folder, '*')):
@@ -13,6 +14,10 @@ for method_folder in glob.glob(os.path.join(folder, '*')):
         # prompt_text = os.path.basename(video_file).split('__')[0].replace('_', ' ')
         task_id = os.path.basename(video_file).split('__')[1].replace('.mp4', '')
         # prompt_mapping[method_name][prompt_text] = task_id
+
+        video = VideoFileClip(video_file)
+        video = video.crop(x1=0, y1=0, width=video.h, height=video.h)
+        video.write_videofile(video_file, verbose=False)
         
         os.rename(video_file, video_file.replace('__' + task_id, ''))
 

093f58c2193445d9906ea3af911a4eb4d0d4e2eb
Pengsheng Guo <pengsheng_guo@apple.com>
2024-01-25 16:36:12 -0800

    update video

--- rename_video.py
+++ rename_video.py
@@ -2,24 +2,29 @@ import glob
 import os
 import json
 from moviepy.editor import VideoFileClip
-folder = 'assets/'
+folder = '2024_01_23_05_57_00-all/'
 prompt_mapping = {}
 for method_folder in glob.glob(os.path.join(folder, '*')):
     method_name = os.path.basename(method_folder)
     # print(method_name)
     # print(os.path.join(method_folder, "*.mp4"))
     # r_prob = []
+    isExist = os.path.exists(method_folder.replace(folder, 'assets/'))
+    if not isExist:
+        # Create a new directory because it does not exist
+        os.makedirs(method_folder.replace(folder, 'assets/'))
     prompt_mapping[method_name] = {}
     for video_file in sorted(glob.glob(os.path.join(method_folder, "*.mp4"))):
         # prompt_text = os.path.basename(video_file).split('__')[0].replace('_', ' ')
         task_id = os.path.basename(video_file).split('__')[1].replace('.mp4', '')
+        new_video_file_name = video_file.replace('__' + task_id, '')
+        # os.rename(video_file, video_file.replace('__' + task_id, ''))
         # prompt_mapping[method_name][prompt_text] = task_id
 
         video = VideoFileClip(video_file)
         video = video.crop(x1=0, y1=0, width=video.h, height=video.h)
-        video.write_videofile(video_file, verbose=False)
+        video.write_videofile(new_video_file_name.replace(folder, 'assets/'), verbose=False, logger=None)
         
-        os.rename(video_file, video_file.replace('__' + task_id, ''))
 
 # print(prompt_mapping.keys())
 

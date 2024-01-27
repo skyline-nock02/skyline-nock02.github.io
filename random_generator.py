9feeb8d37a4cd173ebba61e341e80179629a3943
Pengsheng Guo <pengsheng_guo@apple.com>
2024-01-26 17:50:54 -0800

    add random order

--- random_generator.py
+++ random_generator.py
@@ -10,6 +10,7 @@ total_videos = 40
 total_baselines = 4
 random_baselines = []
 random_prompts = []
+random_orders = []
 seeds = [0,1,2,3,4,5,6,7,8,9,10]
 prompts_array = [i for i in range(total_prompts)]
 
@@ -17,6 +18,8 @@ for seed in seeds:
     random.seed(seed)
     random_baselines.append([random.randint(0, total_baselines-1) for _ in range(total_videos)])
     random_prompts.append(random.sample(prompts_array, total_videos))
+    random_orders.append([random.randint(0, 1) for _ in range(total_videos)])
 
 print(f"random_baselines = {random_baselines}")
 print(f"random_prompts = {random_prompts}")
+print(f"random_orders = {random_orders}")
